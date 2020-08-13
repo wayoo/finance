5523a8e4ae149456a88634628fc8eb5c1fadd53e
wangyuzju <wangyu0248@gmail.com>
2020-08-13 20:19:15 +0800

    fix: bug

--- src/views/word/list.vue
+++ src/views/word/list.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="page-world-list">
-    <card style="width: 350px" v-for="item in list" :key="item">
+    <card style="width: 350px" v-for="item in list" :key="item.word">
       <p slot="title">
             <Icon type="ios-film-outline"></Icon>
-            {{ item }}
-        </p>
-      <img :src="`https://vocab.oss-cn-beijing.aliyuncs.com/${item}.jpg?x-oss-process=image/resize,w_320`" alt="">
+            {{ item.word }}
+      </p>
+      <div v-if="item.desc">
+        {{ item.desc }}
+      </div>
+      <img :src="`https://vocab.oss-cn-beijing.aliyuncs.com/${item.word}.jpg?x-oss-process=image/resize,w_320`" alt="">
     </card>
   </div>
 </template>
@@ -16,14 +19,39 @@ import '../style.less';
 
 export default {
   data() {
+    const list = [
+      'canteen',
+      'confectionery',
+      'tagline',
+      'limbo',
+      'hibernation',
+      'rivalry',
+      'indulgent',
+      'heritage',
+      'wardrobe',
+      'degenerate',
+      'allege',
+      'unprecedented',
+      'vituperation',
+      'boilerplate',
+      'umbrage',
+      'consulate',
+      {
+        word: 'prospectus',
+        desc: 'A prospectus, in finance, is a disclosure document that describes a financial security for potential buyers.',
+      },
+      'consortium',
+    ];
+
     return {
-      list: [
-        'canteen',
-        'confectionery',
-        'tagline',
-        'limbo',
-        'hibernation',
-      ],
+      list: list.map((val) => {
+        if (val.desc) {
+          return val;
+        }
+        return {
+          word: val,
+        };
+      }),
     };
   },
 };
